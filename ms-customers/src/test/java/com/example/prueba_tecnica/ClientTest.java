7ff38798cb2600123973ffc00494ff4d329cf3e3
deyvisgc <deivisgarcia@upeu.edu.pe>
2024-03-29 00:12:07 -0500

    agregando un orden a las pruebas unitarias

--- ms-customers/src/test/java/com/example/prueba_tecnica/ClientTest.java
+++ ms-customers/src/test/java/com/example/prueba_tecnica/ClientTest.java
@@ -19,6 +19,8 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 class ClientTest {
     @Mock
     private ClientRepository clientRepositoryMock;
@@ -28,6 +30,7 @@ class ClientTest {
     ClienteMapper clienteMapperMock = Mappers.getMapper(ClienteMapper.class);
 
     private Client clienteExistente;
+    private Long clientId;
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -35,16 +38,14 @@ class ClientTest {
     }
     private Client crearClienteExistente() {
         Client cliente = new Client();
-        cliente.setId(1L);
-        cliente.setName("Deyvis Ronald Garcia");
+        cliente.setName("Jose Luis Garcia");
         cliente.setGender("Masculino");
         cliente.setAge(30);
         cliente.setStatus(true);
         cliente.setAddress("Lima, Peru");
-        cliente.setIdentification("12345678");
-        cliente.setPhone("91091442");
-        cliente.setPassword("12345");
-        cliente.setStatus(true);
+        cliente.setIdentification("345342");
+        cliente.setPhone("123456789");
+        cliente.setPassword("123456");
         return cliente;
     }
     private Client updateClienteExistente() {
@@ -57,36 +58,24 @@ class ClientTest {
         cliente.setIdentification("12345678");
         cliente.setPhone("91091442");
         cliente.setPassword("12345");
-        cliente.setStatus(true);
         return cliente;
     }
     @Test
+    @Order(1)
     public void testCrearCliente() {
-         when(clientRepositoryMock.findById(1L)).thenReturn(Optional.of(clienteExistente));
+         //when(clientRepositoryMock.findById(clientId)).thenReturn(Optional.of(clienteExistente));
         // Llamar al método del servicio para crear un cliente existente
         ClientDto clienteDtoEsperado = clienteMapperMock.clienteToClienteDTO(clienteExistente);
         System.out.println("Cliente DTO esperado: " + clienteDtoEsperado);
         Client nuevoCliente = clienteMapperMock.clienteDTOtoCliente(clientServiceImp.save(clienteDtoEsperado));
+        clientId = nuevoCliente.getId();
         System.out.println("Nuevo Cliente: " + nuevoCliente);
         // Verificar que el cliente devuelto es el mismo que el cliente existente
         Assertions.assertEquals(clienteExistente, nuevoCliente);
         // Verificar que no se haya llamado al método save del repositorio
         verify(clientRepositoryMock, never()).save(clienteExistente);
     }
-    @Test
-    public void testActualizarCliente() {
-        Client clienteExistenteUpdate = updateClienteExistente();
-        // Configurar el comportamiento del repositorio mock
-        when(clientRepositoryMock.findById(17L)).thenReturn(Optional.of(clienteExistenteUpdate));
-        when(clientRepositoryMock.save(any())).thenReturn(clienteExistenteUpdate);
-        // Llamar al método actualizarCliente con el ID del cliente existente y el DTO actualizado
-        ClientDto clienteDtoRetornado = clientServiceImp.update(17L, clienteMapperMock.clienteToClienteDTO(clienteExistenteUpdate));
-        Client actualizado = clienteMapperMock.clienteDTOtoCliente(clienteDtoRetornado);
-        // Verificar que el método clienteToClienteDTO fue llamado con el cliente actualizado
-        verify(clienteMapperMock).clienteToClienteDTO(clienteExistenteUpdate);
-        // Verificar que el clienteDto devuelto es el mismo que el clienteDto actualizado
-        Assertions.assertEquals(clienteExistenteUpdate, actualizado);
-    }
+    @Order(2)
     @Test
     public void testListarClientes() {
         // Simular el comportamiento del repositorio
@@ -102,24 +91,41 @@ class ClientTest {
         // Verificar que la lista de clientes no esté vacía
         Assertions.assertFalse(clientesListados.isEmpty());
     }
+    @Order(3)
     @Test
     public void testListarClientesById() {
         // Simular el comportamiento del repositorio
         clienteExistente = crearClienteExistente();
-        when(clientRepositoryMock.findById(17L)).thenReturn(Optional.of(clienteExistente));
+        when(clientRepositoryMock.findById(clientId)).thenReturn(Optional.of(clienteExistente));
         // Llamar al método para listar clientes
-         Client clientesListados = clienteMapperMock.clienteDTOtoCliente(clientServiceImp.getById(17L));
+         Client clientesListados = clienteMapperMock.clienteDTOtoCliente(clientServiceImp.getById(clientId));
         // Verificar que se llame al método findAll del repositorio
         //verify(clientRepositoryMock).findById(3L);
         // Verificar que la lista de clientes no esté vacía
         Assertions.assertEquals(clienteExistente, clientesListados);
     }
     @Test
+    @Order(4)
+    public void testActualizarCliente() {
+        Client clienteExistenteUpdate = updateClienteExistente();
+        // Configurar el comportamiento del repositorio mock
+        when(clientRepositoryMock.findById(clientId)).thenReturn(Optional.of(clienteExistenteUpdate));
+        when(clientRepositoryMock.save(any())).thenReturn(clienteExistenteUpdate);
+        // Llamar al método actualizarCliente con el ID del cliente existente y el DTO actualizado
+        ClientDto clienteDtoRetornado = clientServiceImp.update(clientId, clienteMapperMock.clienteToClienteDTO(clienteExistenteUpdate));
+        Client actualizado = clienteMapperMock.clienteDTOtoCliente(clienteDtoRetornado);
+        // Verificar que el método clienteToClienteDTO fue llamado con el cliente actualizado
+        verify(clienteMapperMock).clienteToClienteDTO(clienteExistenteUpdate);
+        // Verificar que el clienteDto devuelto es el mismo que el clienteDto actualizado
+        Assertions.assertEquals(clienteExistenteUpdate, actualizado);
+    }
+    @Test
+    @Order(5)
     public void testEliminarCliente() {
         // Configurar el comportamiento del repositorio mock
-        doNothing().when(clientRepositoryMock).deleteById(16L);
+        doNothing().when(clientRepositoryMock).deleteById(clientId);
         // Llamar al método para eliminar un cliente
-        clientServiceImp.delete(16L);
+        clientServiceImp.delete(clientId);
         // Verificar que se llame al método deleteById del repositorio con el ID correcto
         //verify(clientRepositoryMock).deleteById(3L);
     }
